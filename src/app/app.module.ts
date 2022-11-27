8c98072fa9eb6f7ba5f7cf7618be0197f10b977e
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-11-27 20:19:18 -0300

    nvm installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
