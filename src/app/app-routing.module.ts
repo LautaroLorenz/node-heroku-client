f09847439b4df8c0632b6363d63d457629313d42
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-11-27 20:36:28 -0300

    default menu page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'menu',
+    pathMatch: 'full'
+    
+  }, {
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
